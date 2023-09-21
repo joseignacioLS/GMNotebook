1600d3145ceca2086ad8b3b1a62bdf42ee3be424
Jose Ignacio Labella Sanfrutos <jlabella@emeal.nttdata.com>
2023-09-21 17:53:08 +0200

    feat: toggleable menu and file name

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,7 +1,6 @@
 import { dataI } from "@/context/constants";
 
 export const saveToFile = (filename: string, content: any) => {
-
   const data = JSON.stringify(content, null, 4);
   let element = document.createElement("a");
   element.setAttribute(
@@ -13,23 +12,23 @@ export const saveToFile = (filename: string, content: any) => {
   document.body.appendChild(element);
   element.click();
   document.body.removeChild(element);
-}
+};
 
 export const loadFile = (inputSelector: string, callback: any) => {
   const fileInput = document.querySelector(inputSelector) as any;
-  if (!fileInput) return
+  if (!fileInput) return;
   const file = fileInput.files[0];
   if (!file) return;
   const reader = new FileReader();
   reader.onload = function (e) {
     const fileContents = e.target?.result;
-    if (!fileContents) return
+    if (!fileContents) return;
     try {
       const parsed = JSON.parse(fileContents as string) as dataI;
-      callback(parsed)
+      callback(parsed);
     } catch (err) {
       console.log(err);
     }
   };
   reader.readAsText(file);
-}
+};
