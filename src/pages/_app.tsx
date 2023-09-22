b0f45e3dfd58b3f4996f621f0916621a59bb871d
Jose Ignacio Labella Sanfrutos <jlabella@emeal.nttdata.com>
2023-09-22 09:02:04 +0200

    feat: create modal and implement it in reset flow

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import { DataProvider } from "@/context/data";
+import { ModalProvider } from "@/context/modal";
 import { NavigationProvider } from "@/context/navigation";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <NavigationProvider>
-      <DataProvider>
-        <Component {...pageProps} />
-      </DataProvider>
-    </NavigationProvider>
+    <ModalProvider>
+      <NavigationProvider>
+        <DataProvider>
+          <Component {...pageProps} />
+        </DataProvider>
+      </NavigationProvider>
+    </ModalProvider>
   );
 }
