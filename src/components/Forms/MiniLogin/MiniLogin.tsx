3ec0eb3f171db1aeaa51077fccc7a39cf49f601f
Jose Ignacio Labella Sanfrutos <jlabella@emeal.nttdata.com>
2023-10-19 14:21:19 +0200

    feat: improve minilogin

--- src/components/Forms/MiniLogin/MiniLogin.tsx
+++ src/components/Forms/MiniLogin/MiniLogin.tsx
@@ -1,48 +1,60 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import styles from "./minilogin.module.scss";
 import Button, { behaviourEnum } from "@/components/Button/Button";
 import { loginToServer } from "@/utils/login";
-import { modalContext } from "@/context/modal";
 import { DataContext } from "@/context/data";
 
 const MiniLogin = () => {
   const [expanded, setExpanded] = useState<boolean>(false);
   const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<boolean>(false);
   const { setGmMode, gameName, gmMode } = useContext(DataContext);
-  const { closeModal } = useContext(modalContext);
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
-    if (await loginToServer(gameName, password)) {
-      setGmMode(true);
-      closeModal();
+
+    if (gmMode) {
+      setGmMode(false);
+    } else {
+      if (!expanded) {
+        return setExpanded(true);
+      }
+      if (await loginToServer(gameName, password)) {
+        setGmMode(true);
+        setExpanded((v) => !v);
+      } else {
+        setError(true);
+        setTimeout(() => setError(false), 500);
+      }
+      setPassword("");
     }
   };
 
-  const handleButtonClick = (e: any) => {
-    if (gmMode) {
-      setGmMode(false);
-      setPassword("");
-    } else {
-      setExpanded((v) => !v);
+  const handleInput = (value: string) => {
+    setPassword(value);
+    if (value === "") {
+      setExpanded(false);
     }
   };
 
   return (
     <form className={styles.wrapper} onSubmit={handleSubmit}>
       <input
-        className={`${styles.passwordInput} ${expanded && styles.expanded}`}
+        className={`${styles.passwordInput} 
+        ${expanded && styles.expanded}`}
         placeholder="password"
         type="password"
         value={password}
         onChange={(e) => {
-          setPassword(e.currentTarget.value);
+          handleInput(e.currentTarget.value);
         }}
       />
       <Button
-        addClass={`${styles.button} ${expanded && styles.expanded}`}
+        addClass={`${styles.button} 
+        ${expanded && styles.expanded}  
+        ${error && styles.buzz}`}
         behaviour={gmMode ? behaviourEnum.NEGATIVE : behaviourEnum.NEUTRAL}
-        onClick={handleButtonClick}
+        onClick={() => {}}
       >
         {gmMode ? (
           <img src="/images/unlocked.svg" />
