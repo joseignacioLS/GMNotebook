4428e79db77cc609692f04870e16499872e94fd8
Jose Ignacio Labella Sanfrutos <jlabella@emeal.nttdata.com>
2023-10-24 13:09:55 +0200

    feat: add home button

--- src/components/Page/PageDisplay.tsx
+++ src/components/Page/PageDisplay.tsx
@@ -2,10 +2,13 @@ import React, { ReactElement, useContext, useEffect, useState } from "react";
 import styles from "./pagedisplay.module.scss";
 import { DataContext } from "@/context/data";
 import { processText } from "@/utils/text";
+import Button from "../Button/Button";
+import { useRouter } from "next/router";
 
 const PageDisplay = () => {
-  const { item, updateSelectedNote, gmMode, currentPage } =
+  const { item, updateSelectedNote, gmMode, gameName, currentPage } =
     useContext(DataContext);
+  const router = useRouter();
 
   const [displayText, setDisplayText] = useState<ReactElement[]>([]);
   useEffect(() => {
@@ -16,6 +19,15 @@ const PageDisplay = () => {
   return (
     <div className={`${styles.pageDisplay}`}>
       <div className={styles.titleContainer}>
+        <Button
+          onClick={() => {
+            router.push(`/${gameName}/RootPage`);
+          }}
+          naked={true}
+          disabled={currentPage === "RootPage"}
+        >
+          <img src="/images/house.svg" />
+        </Button>
         <h1 onClick={() => updateSelectedNote(item.key)}>{item.title}</h1>
       </div>
       <div className={styles.text} id="text">
