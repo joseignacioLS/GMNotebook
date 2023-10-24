0964cc2e453b41c7e6c670073abb074f3560b23e
Jose Ignacio Labella Sanfrutos <jlabella@emeal.nttdata.com>
2023-10-24 12:54:37 +0200

    feat: add inclusive sans font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,20 +4,35 @@ import { ModalProvider } from "@/context/modal";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import localFont from "next/font/local";
+
+const inclusiveSans = localFont({ src: "../fonts/InclusiveSans-Regular.ttf" });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>GM Notebook</title>
+        <style jsx global>
+          {`
+            * {
+              font-family: ${inclusiveSans.style.fontFamily};
+            }
+          `}
+        </style>
       </Head>
-      <DarkModeProvider>
-        <ModalProvider>
-          <DataProvider>
-            <Component {...pageProps} />
-          </DataProvider>
-        </ModalProvider>
-      </DarkModeProvider>
+      <div
+        className={inclusiveSans.className}
+        style={{ width: "100%", height: "100%" }}
+      >
+        <DarkModeProvider>
+          <ModalProvider>
+            <DataProvider>
+              <Component {...pageProps} />
+            </DataProvider>
+          </ModalProvider>
+        </DarkModeProvider>
+      </div>
     </>
   );
 }
