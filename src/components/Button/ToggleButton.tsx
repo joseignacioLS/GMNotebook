03cef4ab64646a403c29723f6ccb86b09442e2a7
Jose <ls.joseignacio@gmail.com>
2024-07-04 17:20:30 +0200

    refactor: togglebutton

--- src/components/Button/ToggleButton.tsx
+++ src/components/Button/ToggleButton.tsx
@@ -4,35 +4,22 @@ import Button from "./Button";
 
 interface IProps {
   isOn: boolean;
-  leftOption?: string;
-  rightOption?: string;
   leftButton?: string | ReactElement;
   rightButton?: string | ReactElement;
   onClick?: any;
 }
 const ToggleButton: React.FC<IProps> = ({
   isOn,
-  leftOption,
-  rightOption,
-  leftButton = <div className="roundBlob alert"></div>,
-  rightButton = <div className="roundBlob"></div>,
+  leftButton = <div className="roundBlob"></div>,
+  rightButton = <div className="roundBlob alert"></div>,
   onClick,
 }) => {
   return (
     <Button naked={true} onClick={onClick}>
-      <div className={styles.wrapper}>
-        <span style={{ opacity: isOn ? "1" : ".25" }}>{leftOption}</span>
-        <div className={styles.toggleSurface}>
-          <div
-            className={styles.toggleKnob}
-            style={{
-              transform: isOn ? "translateX(2rem)" : "translateX(0px)",
-            }}
-          >
-            {isOn ? rightButton : leftButton}
-          </div>
+      <div className={`${styles.wrapper} ${isOn && styles.on}`}>
+        <div className={styles.toggleKnob}>
+          {isOn ? rightButton : leftButton}
         </div>
-        <span style={{ opacity: !isOn ? "1" : ".25" }}>{rightOption}</span>
       </div>
     </Button>
   );
