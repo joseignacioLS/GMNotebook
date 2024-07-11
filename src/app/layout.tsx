81f5f457cd4a77ccf40fb91287d4eb689b57b97f
Jose <ls.joseignacio@gmail.com>
2024-07-11 12:28:34 +0200

    add fonts, adjust text sizes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,20 @@
 import { DataProvider } from "@/context/data";
 import { ModalProvider } from "@/context/modal";
 import { NavigationProvider } from "@/context/navigation";
-
+import { Inclusive_Sans, Merriweather } from "next/font/google";
 import "../styles/globals.scss";
 import { ColorProvider } from "@/context/colors";
-import Head from "next/head";
+
+const inclusive = Inclusive_Sans({
+  weight: ["400"],
+  variable: "--font-inclusive",
+  subsets: ["latin"],
+});
+const merriweather = Merriweather({
+  weight: ["400", "700"],
+  variable: "--font-merriweather",
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "GM Notebook",
@@ -21,7 +31,11 @@ export default function RootLayout({
         <ModalProvider>
           <NavigationProvider>
             <DataProvider>
-              <body>{children}</body>
+              <body
+                className={`${inclusive.variable} ${merriweather.variable}`}
+              >
+                {children}
+              </body>
             </DataProvider>
           </NavigationProvider>
         </ModalProvider>
