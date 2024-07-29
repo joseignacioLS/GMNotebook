63f4aa9458bcabb45a217347049ab578d6e99313
Jose <ls.joseignacio@gmail.com>
2024-07-29 16:49:32 +0200

    fix: enable resend confirm request on request error

--- src/components/Modal/ModalDefaults/ModalShare.tsx
+++ src/components/Modal/ModalDefaults/ModalShare.tsx
@@ -130,11 +130,21 @@ export const ModalShare = () => {
         onChange={(e) => {
           setEmail(e.target.value);
         }}
-        disabled={![EStatus.PREREQUEST, EStatus.EMAIL_ERROR].includes(status)}
+        disabled={
+          ![
+            EStatus.PREREQUEST,
+            EStatus.EMAIL_ERROR,
+            EStatus.REQUEST_ERROR,
+          ].includes(status)
+        }
         label="Email"
         type="email"
       />
-      {[EStatus.PREREQUEST, EStatus.EMAIL_ERROR].includes(status) && (
+      {[
+        EStatus.PREREQUEST,
+        EStatus.EMAIL_ERROR,
+        EStatus.REQUEST_ERROR,
+      ].includes(status) && (
         <>
           <Button onClick={handleRequestShare} disabled={loading}>
             Send me a confirmation code
